4700406eee81009df6e039feb95895e6eeaf674a
Morganxyz <rascaypika@gmail.com>
2023-04-18 18:49:43 +0200

    Exercici 7_1_5 fdlp Java

---
+++ src/M7_1_5_main.java
@@ -0,0 +1,12 @@
+
+public class M7_1_5_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for(int i = 100; i >= 0 ;i--) {
+			System.out.println((i % 2 == 0) ? i : "" );
+		}
+	}
+
+}
